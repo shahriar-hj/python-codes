8a1444bd2481dc69e342e34452e1115ad12a3d92
Shahriar-hj <shahriarhajiabbasi@gmail.com>
2019-05-10 11:56:51 +0330

    VARIABLE

---
+++ S01/var_class.py
@@ -0,0 +1,28 @@
+#this file contains different variable types
+
+my_int=102
+print(my_int)
+print(type(my_int))
+
+my_float=102.5
+print(my_float )
+print(type(my_float))
+
+my_str="Hello Class"
+print(my_str)
+print(type(my_str))
+
+my_comp=7+3j
+print(my_comp)
+print(type(my_comp))
+
+my_bool1=True
+my_bool2=False
+print(my_bool1)
+print(my_bool2)
+print(type(my_bool1),type(my_bool2))
+
+my_list=[1,2,4,"what?",102.3,True,False]
+print(my_list)
+print(type(my_list))
+print(my_list[3])
